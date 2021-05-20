8a74ed8b70f5c3a8a1e7c3dbe196472850621fd3
Wellington Lopes <w.camara.lopes@gmail.com>
2021-05-20 09:23:36 -0300

    feat: cast parameter to method's declaration

--- core/Web/Api.php
+++ core/Web/Api.php
@@ -4,14 +4,59 @@ namespace core\Web;
 
 use core\Bases\Resource;
 
+/**
+ * Parse an API request.
+ */
 class Api
 {
+    /**
+     * @var Resource Instance of resource class.
+     */
     private $resource;
+
+    /**
+     * @var string[] Splitted URI params.
+     */
     private $uri;
+
+    /**
+     * @var string HTTP Method used.
+     */
     private $method;
+
+    /**
+     * @var string Resource's method name.
+     */
     private $resourceMethod;
+
+    /**
+     * @var mixed[] Resource's method parameters.
+     */
     private $resourceParams = [];
 
+    /**
+     * @var string Default parameter type.
+     */
+    const DEFAULT_TYPE = "string";
+
+    /**
+     * @var string[] Types to do a simple cast.
+     */
+    const CASTS = [
+        "int",
+        "array",
+        "object",
+        "string",
+        "bool",
+        "float"
+    ];
+
+    /**
+     * Constructor.
+     *
+     * @param Resource $resource Instace of resource class
+     * @param stirng[] $uri Splitted URI params.
+     */
     public function __construct(Resource $resource, array $uri)
     {
         $this->resource = $resource;
@@ -19,8 +64,12 @@ class Api
         $this->method   = strtolower($_SERVER['REQUEST_METHOD']);
 
         $this->fetchRoute();
+        $this->castParams();
     }
 
+    /**
+     * Execute resource API call.
+     */
     public function execute()
     {
         call_user_func_array(
@@ -29,6 +78,38 @@ class Api
         );
     }
 
+    /**
+     * Cast URI parameters to resource's method.
+     */
+    public function castParams()
+    {
+        $reflection   = new \ReflectionMethod($this->resource, $this->resourceMethod);
+        $methodParams = $reflection->getParameters();
+
+        $index = 0;
+        foreach ($this->resourceParams as &$value) {
+            $type      = "string";
+            $paramType = $methodParams[$index]->getType();
+            $type      = $paramType ? $paramType->getName() : $type;
+
+            if (in_array($type, self::CASTS)) {
+                settype($value, $type);
+            } elseif (class_exists($type)) {
+                $new = new $type();
+                if (!method_exists($new, "find")) {
+                    Response::error("Class '{$type}' must have 'find' method to be used as argument!");
+                }
+                $new->find($value);
+                $value = $new;
+            }
+
+            $index++;
+        }
+    }
+
+    /**
+     * Check route called if availables on resources.
+     */
     private function fetchRoute()
     {
         foreach (Route::fetch($this->method) as $baseRoute => $routes) {
@@ -53,7 +134,14 @@ class Api
         }
     }
 
-    private function matchPath(string $path)
+    /**
+     * Verify if passed URI match with a available path.
+     * Load route params to array.
+     *
+     * @param string $path Available system path
+     * @return bool If is compatible
+     */
+    private function matchPath(string $path): bool
     {
         $split = explode("/", $path);
 
